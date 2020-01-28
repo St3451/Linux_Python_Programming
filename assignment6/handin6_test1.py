5d69504839775a5898c229b57bbae1bb1bb5a230
Stefano Pellegrini <stefano.pellegrini26@gmail.com>
2020-01-28 20:53:38 +0100

    Adding folders

--- assignment6/handin6_test1.py
+++
@@ -1,8 +0,0 @@
-# 3. In the file called handin6_test1.py, call the wordfile_differences_linear_search on the input files, using british-english as file1 and american-english as file2 (it is ok to test it on the test files first, but please switch to the full files before submitting), and saves the result in a variable called differences. 
-import timeit
-import handin6
-start_time = timeit.default_timer()	# write code you want to measure execution time for here
-differences = handin6.wordfile_differences_linear_search("british-english","american-english")
-
-time_spent = timeit.default_timer() - start_time
-print(time_spent)
--- assignment6/handin6_test3.py
+++
@@ -1,9 +0,0 @@
-# 8. The test code in handin6_test3.py should be similar to the two others, but now call the wordfile_differences_dict_search on the input files.
-import timeit
-import handin6
-start_time = timeit.default_timer()
-# write code you want to measure execution time for here
-differences = handin6.wordfile_differences_dict_search("british-english","american-english")
-#print(differences)
-time_spent = timeit.default_timer() - start_time
-print(time_spent)
